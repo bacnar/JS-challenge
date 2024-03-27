e12589c06ffb3f6d574dea684fdbd36b0c5ab195
Rok Bacnar <rok.bacnar@gmail.com>
2024-03-27 19:49:45 +0100

    Refactored validator

--- src/middleware/requestCounter.js
+++ src/middleware/requestCounter.js
@@ -3,22 +3,27 @@ const fs = require('fs/promises');
 
 module.exports = asyncHandler(async (req, res, next) => {
   const filePath = './counter.txt';
-  // Properly implement try catch
   try {
     try {
       await fs.access(filePath);
+
       const rawData = await fs.readFile(filePath);
       const data = JSON.parse(rawData);
+
       data.counter++;
       await fs.writeFile(filePath, JSON.stringify(data));
-    } catch {
-      const data = {
-        counter: 1
-      };
-      await fs.writeFile(filePath, JSON.stringify(data));
+    } catch (exception) {
+      if (exception.code === 'ENOENT' && exception.syscall === 'access') {
+        const data = {
+          counter: 1
+        };
+        await fs.writeFile(filePath, JSON.stringify(data));
+      } else {
+        throw exception;
+      }
     }
-  } catch (err) {
-    console.error('Error while writing counter:', err);
+  } catch (exception) {
+    console.error('Error while writing counter:', exception);
   }
 
   next();
